2884317da6420e0002f90ed485e0dd5590d169a0
keigo <k.kamata.sys23@morijyobi.ac.jp>
2023-10-05 10:41:42 +0900

    pra2.java追記し直し

--- pra2.java
+++ pra2.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             System.out.println("Hallo World");
         }
     }
