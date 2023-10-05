2d01ebca8442c81bc62bc9de375bc08d1658cfd7
keigo <k.kamata.sys23@morijyobi.ac.jp>
2023-10-05 10:36:16 +0900

    「pra2.java」の追記

--- pra2.java
+++ pra2.java
@@ -1,3 +1,7 @@
-public static void main{
-	System.out.println("Hello World");
-}
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Hallo World");
+        }
+    }
+
